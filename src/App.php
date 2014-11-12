f2d74b8f0fb900103d1ff8da426286bc5ab4d134
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2014-11-12 23:37:27 +0900

    install BEAR.Sunday

---
+++ src/App.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Qck\Manivo;
+
+use BEAR\Package\Provide\Application\AbstractApp;
+
+final class App extends AbstractApp
+{
+}
---
+++ src/Module/App/Aspect.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Qck\Manivo\Module\App;
+
+use BEAR\Package;
+use Ray\Di\AbstractModule;
+
+class Aspect extends AbstractModule
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        /*
+        $this->bindInterceptor(
+             $this->matcher->any(),
+             $this->matcher->annotatedWith('Qck\Manivo\Annotation\Bar'),
+             [$this->requestInjection('Qck\Manivo\Interceptor\FooInterceptor')]
+        );
+        */
+    }
+}
