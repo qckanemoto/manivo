5b2dc98b022912ffc45dcfcc8984bf3b2cb5f6de
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2014-11-13 23:44:04 +0900

    add User resource

--- src/App.php
+++ src/App.php
@@ -3,7 +3,20 @@
 namespace Qck\Manivo;
 
 use BEAR\Package\Provide\Application\AbstractApp;
+use Parse\ParseClient;
+use Ray\Di\Di\PostConstruct;
 
 final class App extends AbstractApp
 {
+    /**
+     * @PostConstruct()
+     */
+    public function initParse()
+    {
+        $appId = 'JgytQlORxrBx6B7uNd757Z9JlIhZCCQJvoBN8fG1';
+        $restKey = 'dChlqyK9E0K6LlYrPBVSkz7vtSr1FI8pDJHumEKa';
+        $masterKey = 'qNJfKMX7H67ZUraU0yodiVoudeyVgkrYJctsKRIT';
+
+        ParseClient::initialize($appId, $restKey, $masterKey);
+    }
 }
