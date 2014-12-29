3cd72d6654c8be2726ce8afb314383509c8f5379
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2014-12-29 22:24:34 +0900

    add Interceptor and error app resource

---
+++ src/Resource/App/Exception/Parse.php
@@ -0,0 +1,25 @@
+<?php
+namespace Qck\Manivo\Resource\App\Exception;
+
+use BEAR\Resource\ResourceObject;
+
+class Parse extends ResourceObject
+{
+    /**
+     * @param $statusCode
+     * @param $exceptionCode
+     * @param $exceptionMessage
+     * @return $this
+     */
+    public function onGet($statusCode, $exceptionCode, $exceptionMessage)
+    {
+        $this->code = $statusCode;
+
+        $this['error'] = [
+            'code' => $exceptionCode,
+            'message' => $exceptionMessage,
+        ];
+
+        return $this;
+    }
+}
